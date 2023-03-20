04b6bd190416221d6fac46348815195c65e6336c
Fran <francocaminosmail@gmail.com>
2023-03-20 14:07:49 -0300

    added enviroment w/ socket io

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require("socket.io")(http, {
+	cors: {
+		origins: [
+			"http://localhost:8080",
+		],
+	},
+});
+
+io.use((socket, next) => {
+  const token = socket.handshake.auth.token;
+  console.log('token', token);
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hey Socket.io</h1>');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('my message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('my broadcast', `server: ${msg}`);
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
---
+++ src/services/socketio.service.js
@@ -0,0 +1,23 @@
+import { io } from 'socket.io-client';
+
+class SocketioService {
+  socket;
+  constructor() {}
+
+  setupSocketConnection() {
+    this.socket = io("http://localhost:3000");
+    this.socket.emit('my message', 'Hello there from Vue.');
+    
+    this.socket.on('my broadcast', (data) => {
+      console.log(data);
+    });
+  }
+  
+  disconnect() {
+    if(this.socket) {
+      this.socket.disconnect();
+    }
+  }
+}
+
+export default new SocketioService();
